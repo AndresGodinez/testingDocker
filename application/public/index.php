d48b0e89b7c7444c469b8338f0efc23c94f4d159
AGodinez <ing.a.godinez@gmail.com>
2019-03-31 09:55:20 -0600

    adding notes on project

--- application/public/index.php
+++ application/public/index.php
@@ -1,2 +1,19 @@
 <?php
-phpinfo();
+echo "Hostname 'redis' can be found at ".gethostbyname('redis'). "\n";
+
+$hostname='mysql';
+$username='andres';
+$password='secret';
+$dbname="testing";
+
+try{
+    $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password, 
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    echo "Conectado a 'mysql in ". gethostbyname('mysql'). "\n";
+
+}catch(Exeption $e){
+    echo "no se ha podido establecer la conexion";
+    echo $e->getMessage();
+}
